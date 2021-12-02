abcf566fe76a16906169f47e4269ade3aa71a5ce
Faustin LEPERON <faleperon@IUTCLINFA2319.iut.local>
2021-12-02 12:02:11 +0100

    Début vueAccueil

---
+++ code/src/Launcher/Launcher.java
@@ -0,0 +1,17 @@
+package Launcher;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class Launcher extends Application
+{
+    public void start(Stage stage) throws IOException {
+        stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("/fxml/vueAccueil.fxml"))));
+        stage.setMaximized(true);
+        stage.show();
+    }
+}
