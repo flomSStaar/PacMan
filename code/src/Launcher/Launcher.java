17250a15919192ad5f89e91803b082cdff568cc1
Faustin LEPERON <faleperon@IUTCLINFA2319.iut.local>
2021-12-09 12:00:54 +0100

    Mofification de la vue d'accuille et de la vue de parametre

--- code/src/Launcher/Launcher.java
+++ code/src/Launcher/Launcher.java
@@ -3,6 +3,7 @@ package Launcher;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.input.KeyCombination;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,7 +12,9 @@ import java.util.Objects;
 public class Launcher extends Application {
     public void start(Stage stage) throws IOException {
         stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/vueAccueil.fxml")))));
-        stage.setMaximized(true);
+        stage.setFullScreen(true);
+        stage.setFullScreenExitHint("");
+        //stage.setFullScreenExitKeyCombination(KeyCombination.NO_MATCH);
         stage.show();
     }
 }
