062713eb3100d25b5f82ac8f3cb68b34e699f0ef
faleperon <faustin.leperon@etu.uca.fr>
2021-12-16 22:37:59 +0100

    j'ai reussi

--- code/src/Launcher/Launcher.java
+++ code/src/Launcher/Launcher.java
@@ -13,21 +13,17 @@ import model.entity.Wall;
 
 import javafx.scene.image.ImageView;
 import javax.swing.text.html.parser.Entity;
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 
 public class Launcher extends Application {
-    public void start(Stage stage) throws IOException {
+    public void start(Stage stage) throws Exception {
         //stage.setScene(new Scene(FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxml/vueJeu.fxml")))));
         List<Wall> walls = new ArrayList<>();
         List<PacMan> pacmans = new ArrayList<>();
         readFile(walls, pacmans);
-        getClass().getResource("text/map.txt");
         stage.setScene(getSce(walls, pacmans));
         stage.setFullScreen(true);
         stage.setFullScreenExitHint("");
@@ -43,15 +39,15 @@ public class Launcher extends Application {
             pane.getChildren().addAll(i);
         }
         for (PacMan entity : pacmans) {
-            ImageView i = new ImageView(new Image("/image/PacManSprite.jpg"));
+            ImageView i = new ImageView(new Image("/image/PacManSprite.png"));
             i.relocate(entity.getX(), entity.getY());
             pane.getChildren().addAll(i);
         }
         return new Scene(pane);
     }
 
-    public void readFile(List<Wall> walls, List<PacMan> pacmans) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader(getClass().getResourceAsStream("/text/map.txt")));
+    public void readFile(List<Wall> walls, List<PacMan> pacmans) throws Exception {
+        BufferedReader br = new BufferedReader(new FileReader(new File(getClass().getResource("/text/map.txt").toURI())));
         int ligne = 0, collone = 0, c = 0;
         while ((c = br.read()) != -1) {
             switch ((char)c){
@@ -60,12 +56,16 @@ public class Launcher extends Application {
                     ligne += 45;
                     break;
                 case '1':
-                    collone +=  45;
                     walls.add(new Wall(collone, ligne, 45, 45));
+                    collone +=  45;
                     break;
                 case '2':
-                    collone +=  45;
                     pacmans.add(new PacMan(collone, ligne, 45, 45));
+                    collone +=  45;
+                    break;
+
+                case '0':
+                    collone +=  45;
                     break;
             }
         }
